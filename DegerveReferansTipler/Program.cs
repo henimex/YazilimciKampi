5dcfe3b111a4bb57658923bdc83b8e8da60675e1
henimex <49072861+henimex@users.noreply.github.com>
2021-01-17 17:29:37 +0300

    Kamp 3.Gun

--- DegerveReferansTipler/Program.cs
+++ DegerveReferansTipler/Program.cs
@@ -14,6 +14,7 @@ namespace DegerveReferansTipler
             sayi1 = sayi2;
             sayi2 = 65;
             Console.WriteLine(sayi1);
+            //30
 
             int [] sayilar1 = new int[] { 10, 20, 30 };
             int [] sayilar2 = new int[] { 100, 200, 300 };
@@ -21,6 +22,7 @@ namespace DegerveReferansTipler
             sayilar1 = sayilar2;
             sayilar2[0] = 999;
             Console.WriteLine(sayilar1[0]);
+            //999
         }
     }
 }
